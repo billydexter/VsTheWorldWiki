fffe17ea33817a9d517f87b19f969f9d834de8c8
Nathan Driggs <driggs.nathan@gmail.com>
2019-05-08 22:42:16 -0600

    Started the project and added many of the files

---
+++ js/Characters/characterScript.js
@@ -0,0 +1,39 @@
+window.onload = function() 
+{
+	document.getElementById("spoilButton0").addEventListener('click', function()
+	{
+		document.getElementById("spoilWarning0").style.display = "none";
+		document.getElementById("spoilText0").style.display = "block";
+		document.getElementById("spoilButton0").style.display = "none";
+	});
+	document.getElementById("spoilButton1").addEventListener('click', function()
+	{
+		document.getElementById("spoilWarning1").style.display = "none";
+		document.getElementById("spoilText1").style.display = "block";
+		document.getElementById("spoilButton1").style.display = "none";
+	});
+	document.getElementById("spoilButton2").addEventListener('click', function()
+	{
+		document.getElementById("spoilWarning2").style.display = "none";
+		document.getElementById("spoilText2").style.display = "block";
+		document.getElementById("spoilButton2").style.display = "none";
+	});
+	document.getElementById("spoilButton3").addEventListener('click', function()
+	{
+		document.getElementById("spoilWarning3").style.display = "none";
+		document.getElementById("spoilText3").style.display = "block";
+		document.getElementById("spoilButton3").style.display = "none";
+	});
+	document.getElementById("spoilButton4").addEventListener('click', function()
+	{
+		document.getElementById("spoilWarning4").style.display = "none";
+		document.getElementById("spoilText4").style.display = "block";
+		document.getElementById("spoilButton4").style.display = "none";
+	});
+	document.getElementById("spoilButton5").addEventListener('click', function()
+	{
+		document.getElementById("spoilWarning5").style.display = "none";
+		document.getElementById("spoilText5").style.display = "block";
+		document.getElementById("spoilButton5").style.display = "none";
+	});
+}
---
+++ js/script.js
@@ -0,0 +1,9 @@
+window.onload = function() {
+
+}
+
+function revealChapter12Spoilers()
+		{
+			document.getElementById("chapter12Spoilers").style.display = "none";
+			document.getElementById("chapter12Destyl").style.display = "block";
+		}
